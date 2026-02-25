5139a6cbc053d27106ae40671611324977bb1ca6
AbdelrahmanSamirz <shekaba@gmail.com>
2026-02-25 21:27:55 +0200

    EncapculatonSetterGetter

---
+++ BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoSession02_OOP_
+{
+    internal class BankAccount
+    {
+        #region Fields
+        //private int _AccountNumber;
+        //private decimal _Balance;
+        
+        //private string _Name;
+
+        #endregion
+
+        #region Methods
+
+        //public void SetBalance(decimal balance)
+        //{
+        //    if (balance < 0)
+        //    {
+        //        Console.WriteLine("Invalid Negative Number");
+        //        return;
+        //    }
+        //    _Balance = balance;
+        //}
+        //public decimal GetBalance()
+        //{
+        //    return _Balance;
+        //}
+        //public int GetAccountNumber() //Read
+        //{
+        //    return _AccountNumber;
+        //}
+
+        //public void SetName(string value) 
+        //{   
+        //    _Name = value;
+        //}
+        #endregion
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Encapculation Using Setter And Getter
+            //BankAccount Acc01 = new BankAccount();
+            //Acc01.SetName("Sheka");
+            //Acc01.SetBalance(-50);
+            //Console.WriteLine(Acc01.GetBalance());
+            #endregion
         }
     }
 }
