a8eff2ab62438992cf8c9d0ce7d3a5a9f3de5803
AbdelrahmanSamirz <shekaba@gmail.com>
2026-02-25 20:54:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoSession02_OOP_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
